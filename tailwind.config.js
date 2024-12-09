64c2e5be5c7e10ecf0d32c72f048399eed6cb352
zmudzkik <zmudzki.kamil2@gmail.com>
2024-12-10 00:40:10 +0100

    created color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      primary: "#01473E",
+      "primary-light": "#014B41",
+      secondary: "#F2F8F0",
+      "secondary-medium": "#ECF5EA",
+      "secondary-dark": "#E4EEE6",
+      grey: "#A0BCB3",
+      "grey-medium": "#98ADAA",
+    },
     extend: {},
   },
   plugins: [],
