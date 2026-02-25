687ccb7ae2fd4e95aaced3e410061daa2cc9ee21
zmudzkik <zmudzki.kamil2@gmail.com>
2026-02-25 23:56:25 +0100

    changed logo

--- src/layout/components/logo.tsx
+++ src/layout/components/logo.tsx
@@ -6,9 +6,17 @@ export const Logo: React.FC<Props> = () => {
   return (
     <Link
       to="/"
-      className="flex items-center text-secondary h-full px-4 font-zendots text-[2.625rem]"
+      className="flex flex-col justify-center text-secondary h-full px-4"
     >
-      ZK.dev
+      <span className="flex items-baseline text-[2.625rem] font-zendots leading-none">
+        ZK
+        <span className="text-secondary/40 tracking-tighter text-[2.25rem]">
+          amil
+        </span>
+      </span>
+      <span className="text-xs tracking-[0.5em] text-secondary/60">
+        DEVELOPMENT
+      </span>
     </Link>
   );
 };
