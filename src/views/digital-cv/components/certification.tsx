9d0ceff6dc535cbe4f064604449eadae7b54a398
zmudzkik <zmudzki.kamil2@gmail.com>
2026-02-26 21:44:30 +0100

    fix: certifications on mobiles

--- src/views/digital-cv/components/certification.tsx
+++ src/views/digital-cv/components/certification.tsx
@@ -8,9 +8,9 @@ interface Props {
 
 export const Certification: React.FC<Props> = ({ cert }) => {
   return (
-    <div className="flex gap-10 items-center w-full h-48 bg-secondary-dark p-6 rounded-2xl">
+    <div className="flex flex-col sm:flex-row gap-6 sm:gap-10 items-center w-full min-h-48 bg-secondary-dark p-6 rounded-2xl">
       <Icon variant="primary" iconClassName={cert.icon} size="large" />
-      <div className="flex flex-col gap-1">
+      <div className="flex flex-col items-center sm:items-start text-center sm:text-start gap-4 sm:gap-1">
         <h3 className="text-3xl font-bold">{cert.name}</h3>
         <p className="text-black/70">{cert.description}</p>
         <div className="flex flex-row gap-10">
